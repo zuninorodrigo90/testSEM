46f2b42b3595e891148d4b4d33ed03a54abe07b5
Rodrigo Zunino <rodrigo.zunino@grainger.com>
2021-04-07 19:37:39 -0300

    Finished SEM

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,35 @@
 package main;
 
+import java.util.Stack;
+
 public class Main {
-    public static void main (String [] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) {
+        int vec[] = {11, 13, 21, 3};
+        printSEM(vec);
+    }
+
+    public static void printSEM(int vec[]) {
+        if (vec.length == 0) {
+            System.out.println("No elements");
+            return;
+        }
+        Stack<Integer> stack = new Stack();
+        stack.push(vec[0]);
+        for (int i = 1; i < vec.length; i++) {
+            int currentVec = vec[i];
+
+            while (!stack.isEmpty()) {
+                int elementStack = stack.pop();
+                if (currentVec > elementStack) {
+                    System.out.println(elementStack + "->" + currentVec);
+                } else {
+                    stack.push(elementStack);
+                    break;
+                }
+            }
+            stack.push(currentVec);
+        }
+        stack.stream().map(n -> n + "".concat("-> -1")).forEach(System.out::println);
+
     }
 }
