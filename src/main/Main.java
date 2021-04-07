6bd8e19dccb9840f50de5a1b71d91bf763c6f274
Rodrigo Zunino <rodrigo.zunino@grainger.com>
2021-04-07 19:54:53 -0300

    Added PDF with execises and txt with results

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        int vec[] = {11, 13, 21, 3};
+        int vec[] = {13, 7, 6, 12};
         printSEM(vec);
     }
 
